11487b5597edca27ca1a55d52f149570be032240
Sekai Alien <sekai.alien@mail.ru>
2022-11-14 18:20:55 +0500

    task 34 - even 3-diigit numbers in array

---
+++ task-34/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int [] array = new int [4];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);    
+}
+Console.WriteLine(String.Join(", ", array));
+
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i]%2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
